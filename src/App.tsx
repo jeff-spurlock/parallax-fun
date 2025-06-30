4dea11e65007cab55de895ae655342648fe36ccd
jeff-spurlock <jeffspurlock@icloud.com>
2025-06-29 19:23:09 -0500

    basic concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useParallax } from 'react-scroll-parallax'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const items = Array.from({ length: 300 }).map(() => {
+    const x = Math.floor(Math.random() * 100)
+    const y = Math.floor(Math.random() * 100)
+    const z = Math.floor(Math.random() * 6)
+    return { x, y, z }
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-[300vh] bg-gray-900 relative">
+      <h1 className="text-white text-2xl p-4">Test</h1>
+      <div className="relative w-full h-full">
+        {items.map((item, index) => (
+          <Dot key={index} {...item} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  )
+}
+
+const Dot = ({ x, y, z }: { x: number, y: number, z: number }) => {
+  const parallax = useParallax({ speed: (z * 10)})
+  return (
+    <div
+      ref={parallax.ref as React.Ref<HTMLDivElement>}
+      className="w-4 h-4 rounded-full bg-red-500 absolute"
+      style={{
+        left: `${x}%`,
+        top: `${y}%`,
+        scale: `${z * 20 + 75}%`
+      }}
+    />
   )
 }
 
